855aae825e23212d9946bf609932ba7238a963f2
Miscanthus40076 <mangji903@gmail.com>
2026-03-30 09:27:55 +0800

    Add camera integrations and related script wiring

---
+++ src/lerobot/cameras/orbbec/__init__.py
@@ -0,0 +1,9 @@
+from .camera_orbbec import (
+    OrbbecColorCamera,
+    OrbbecDepthCamera,
+    SharedOrbbecColorCamera,
+    SharedOrbbecDepthCamera,
+    SharedOrbbecManager,
+    find_orbbec_cameras,
+)
+from .configuration_orbbec import OrbbecColorCameraConfig, OrbbecDepthCameraConfig
